ca3bb38b410b720a284831a0d7ffe03515545b06
Leo Hammett <leo@hammett.biz>
2025-10-17 07:45:19 +0100

    Ok it's beginning to be nice now

---
+++ lua/custom/plugins/nabla.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    'williamboman/mason.nvim',
+    opts = { ensure_installed = { 'tree-sitter-cli' } },
+  },
+
+  {
+    'jbyuki/nabla.nvim',
+    dependencies = {
+      'nvim-neo-tree/neo-tree.nvim',
+      'williamboman/mason.nvim',
+    },
+    lazy = true,
+
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = { 'latex' },
+        auto_install = true,
+        sync_install = false,
+      }
+    end,
+
+    keys = function()
+      return {
+        {
+          '<leader>p',
+          ':lua require("nabla").popup()<cr>',
+          desc = 'NablaPopUp',
+        },
+      }
+    end,
+  },
+}
